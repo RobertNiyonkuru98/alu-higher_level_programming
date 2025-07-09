f320fa86ea65f72cdb37489d5f59abfa741bfecb
Robert <r.niyonkuru@alustudent.com>
2025-07-09 17:06:29 +0200

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for item in my_list[:x]:
-        print(item, end="")
-        count += 1
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except:
+        pass
     print()
     return count
