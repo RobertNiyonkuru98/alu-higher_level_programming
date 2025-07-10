ed669e80b042b6799d3efb80e3bc39f97845dd0c
Robert <r.niyonkuru@alustudent.com>
2025-07-10 22:43:14 +0200

    Classes and Objects

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Defining a class called square"""
 
+
+class Square:
+    """Defines a Square"""
+
 def __init__(self, size=0):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
