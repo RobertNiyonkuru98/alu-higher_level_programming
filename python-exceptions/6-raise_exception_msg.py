bc2c16c1e7af4e2fc7f2eafac8dec177dee4f96d
Robert <r.niyonkuru@alustudent.com>
2025-07-10 08:53:06 +0200

    Python Exceptions

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        return raise_exception_msg()
-    except NameError:
-        print("Error Message")
+    raise NameError(message)
