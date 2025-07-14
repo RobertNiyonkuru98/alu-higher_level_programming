daceadf045e6aa325a27e67c293c617aaa58e41e
Robert <r.niyonkuru@alustudent.com>
2025-07-14 13:36:22 +0200

    Tony Commits

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that writes into a string into a file utf-8"""
+
+
+def write_file(filename="", text=""):
+    """Writing inside the file"""
+    with open(filename, "w",encoding="utf-8") as file:
+        content = file.write(text)
+    print(content, end="")
