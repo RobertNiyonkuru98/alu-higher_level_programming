df068c9667ad6c700444a257cc0e9152fea24d26
Robert <r.niyonkuru@alustudent.com>
2025-07-15 10:03:59 +0200

    Tony Commits

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 
 import sys
 import json
-dumps_in_5 = __import__("5-save_to_json_file").save_to_json_file
-loads_in_6 = __import__("6-load_from_json_file").load_from_json_file
-
+dumps_in_5 = __import__('5-save_to_json_file').save_to_json_file
+loads_in_6 = __import__('6-load_from_json_file').load_from_json_file
+""""""
 try:
     json_list = loads_in_6("add_item.json")
 except FileNotFoundError:
