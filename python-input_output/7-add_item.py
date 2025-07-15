b46d73269360b3dd668acc1b94fddfb0fa2afd24
RobertNiyonkuru98 <r.niyonkuru@alustudent.com>
2025-07-15 18:41:28 +0200

    Python Input and Output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""A script using Functions to add imported arguments to a list"""
+'''script that adds all args to a python list and saves them to a file'''
 
 import sys
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -9,5 +10,5 @@ try:
     json_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     json_list = []
-json_list = json_list + sys.argv[1:]
+json_list += sys.argv[1:]
 save_to_json_file(json_list, "add_item.json")
