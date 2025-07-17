4e4f370603e6dd21adac646f0bd800e6663c3bb3
Robert Niyonkuru <r.niyonkuru@alustudent.com>
2025-07-17 04:13:19 -0700

    Python Input and Output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,10 @@ load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
 save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-""""""
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:
     items = []
-
 items.extend(sys.argv[1:])
-
 save_to_json_file(items, filename)
 
