11f730bd77267de555daacbbcb78139424837cd8
Robert Niyonkuru <r.niyonkuru@alustudent.com>
2025-07-17 04:11:56 -0700

    Python Input and Output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-Script that adds all command line arguments to a Python list
-stored in a JSON file.
-"""
+"""Script that adds all command line arguments to a Python list stored in a JSON file."""
 
 import sys
 load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
 save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-
+""""""
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:
@@ -18,3 +15,4 @@ except FileNotFoundError:
 items.extend(sys.argv[1:])
 
 save_to_json_file(items, filename)
+
