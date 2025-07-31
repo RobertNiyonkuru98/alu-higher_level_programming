9ceb3b6dbdc712d48594add5e69db97aa282b009
Robert <r.niyonkuru@alustudent.com>
2025-07-31 10:02:15 +0200

    Python Network 1

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = "https://intranet.hbtn.io/status"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    '\n ApplewebKit/537.36 (KHTML, like Gecko) '
+    '\n Chrome/99.0.4844.84 Safari/537.36',
+}
+
+req = urllib.request.Request(url, headers=headers)
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
