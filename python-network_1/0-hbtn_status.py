fd72f5db725987d76ec7bf09358edee370b82323
Robert <r.niyonkuru@alustudent.com>
2025-07-31 21:25:29 +0200

    Python Network 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,14 +5,14 @@ Module 0-hbtn_status.py
 
 
 import urllib.request
-""" """
+
 url = 'https://intranet.hbtn.io/status'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
     '\n    Chrome/99.0.4844.84 Safari/537.36',
 }
-""" """
+
 req = urllib.request.Request(url, headers=headers)
 with urllib.request.urlopen(req) as response:
     content = response.read()
