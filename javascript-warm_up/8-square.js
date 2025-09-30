60860b14452115cb30de1c73fee47da2a247a56a
Robert Niyonkuru <r.niyonkuru@alustudent.com>
2025-09-30 04:15:47 -0700

    Tony commits

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,11 +2,11 @@
 const num = parseInt(process.argv[2]);
 
 if (num) {
-	for (let i = 0; i < num; i++) {
-		console.log('X'.repeat(num));
-	}
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 } else if (num < 0) {
-	console.log();
+  console.log();
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
