ce07b1f8a8bb9d83534e2a4eac459b1280a18338
Robert Niyonkuru <r.niyonkuru@alustudent.com>
2025-09-30 04:08:37 -0700

    Tony commits

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,6 +3,6 @@ function factorial (num) {
 	if (isNan(num) || num <= 1) {
 		return 1;
 	}
-	return num = factorial(num - 1); 
+	return num * factorial(num - 1); 
 }
 console.log(factorial(parseInt(process.argv[2])));
